a343589dbc28e0d59793484994c6b0e19056d454
Vidul Nikolaev Petrov <vidul@cpan.org>
2002-10-19 16:13:37 +0000

    Import of VIDUL/Linux-usermod-0.3 from CPAN.
    gitpan-cpan-distribution: Linux-usermod
    gitpan-cpan-version:      0.3
    gitpan-cpan-path:         VIDUL/Linux-usermod-0.3.tar.gz
    gitpan-cpan-author:       VIDUL
    gitpan-cpan-maturity:     released

--- usermod.pm
+++ usermod.pm
@@ -3,7 +3,7 @@ package Linux::usermod;
 use strict;
 use Carp;
 use vars qw($VERSION);
-$VERSION = 0.2;
+$VERSION = 0.3;
 
 
 
@@ -90,8 +90,17 @@ sub change {
 		if(/^$username:/){
 	  		@fields = split(/:/, $_, 8);
 			CASE: {
-				if($field eq "password") { $usr->{"password"} = ($fields[1] = $new_value ?
-					crypt($new_value, substr($fields[1], 0, $salt)) : $fields[1]);
+				if($field eq "password") {
+					if($fields[1] =~ /^!/) { 
+					  $fields[1] =~ s/^!//;
+					  ($fields[1] = $new_value ?
+					  crypt($new_value, substr($fields[1], 0, $salt))
+					  : $fields[1]) =~ s/(.*)/!$1/;
+					  $usr->{"password"} = $fields[1];
+					} else { 
+					  $usr->{"password"} = ($fields[1] = $new_value ?
+				  	  crypt($new_value, substr($fields[1], 0, $salt)) : $fields[1]);
+					}
 					last CASE
 				}
 				if($field eq "password_l") { 
@@ -131,6 +140,69 @@ sub change {
 	truncate(FH, tell(FH)) or croak "cannot truncate shadow file\n";
 	close(FH);
 	return $usr->{$field};
+ } elsif($field eq "name") {
+	my $sfile = "/etc/shadow";
+	my $pfile = "/etc/passwd";
+	if(!$new_value) {
+		open(FH, "<$pfile") or croak "cannot open /etc/passwd\n";
+		flock(FH, 2) or croak "cannot lock /etc/passwd\n";
+		@F = <FH>;
+		close(FH);
+		for(@F) {
+		 if(/^\Q$username\E:/){
+			return $username;
+		 }
+		}
+	} else {
+		open(FH, "<$pfile") or croak "cannot open /etc/passwd\n";
+		flock(FH, 2) or croak "cannot lock /etc/passwd\n";
+		@F = <FH>;
+		close(FH);
+		for(@F) {
+			if(/^\Q$new_value\E:/) {
+				croak "username already in use\n"
+			}
+		}
+	}
+	open(FH, "+<$pfile") or croak "cannot open /etc/passwd\n";
+	flock(FH, 2) or croak "cannot lock /etc/passwd\n";
+	@F = <FH>;
+	for (@F) {
+		if(/^$username:/){
+			@fields = split(/:/, $_);
+			$usr->{"name"} = ($fields[0] = $new_value ? $new_value : $fields[0]);
+			$fields = join(":", @fields);
+			$fields =~ s/\n//
+			}
+	}
+	for(@F) { if(/^$username:/){  s/.*/$fields/; } }
+	seek(FH, 0, 0) or croak "cannot seek passwd file\n";
+	print FH @F or croak "cannot write to passwd file\n";
+	truncate(FH, tell(FH)) or croak "cannot truncate passwd file\n";
+	close(FH);
+	open(FH, "+<$sfile") or croak "cannot open /etc/shadow\n";
+	flock(FH, 2) or croak "cannot lock /etc/shadow\n";
+	@F = <FH>;
+	for(@F) {
+	   if($_ =~ /^\Q$new_value\E^:/) {
+		croak "name already in use\n";
+	   }
+	}
+
+	for (@F) {
+		if(/^$username:/){
+			@fields = split(/:/, $_);
+			$fields[0] = $new_value ? $new_value : $fields[0];
+			}
+			$fields = join(":", @fields);
+			$fields =~ s/\n//;
+	}
+	for(@F) { if(/^$username:/){ s/.*/$fields/ } }
+	seek(FH, 0, 0) or croak "cannot seek shadow file\n";
+	print FH @F or croak "cannot write to shadow file\n";
+	truncate(FH, tell(FH)) or croak "cannot truncate shadow file\n";
+	close(FH);
+	return $usr->{"name"};
  } else { return "Illegal field name!\n" }
 }
 sub lock {
@@ -142,6 +214,10 @@ sub unlock {
 	my($usr) = shift;
 	$usr->change("password_u");
 	return $usr->change("password");
+}
+sub authors {
+	my $pkg = shift;
+	print "Thanks to the authors of Perl and Linux!\n"
 }
 
 1;
@@ -157,7 +233,7 @@ Linux::usermod - modify user accounts
   use Linux::usermod;
 
   $user = Linux::usermod->new("username");
-  
+
   $user->change($field_name, $new_value);
   $user->show($field_name);
   $user->lock;
@@ -168,7 +244,7 @@ Linux::usermod - modify user accounts
 B<Linux::usermod> is a simple package which change or return fields from B</etc/passwd> and B</etc/shadow>
 files. It acts like  B<usermod> (with the main exception it is OOPerl program).
 The job is done entirely in B<change> which returns(B<show>s) the value of the field specified as a first argument. The second optional argument serves as a new value for the field. If password field is specified, You
-can use third argument for the value of B<salt> which is 13 characters by default. The package implements a 
+can use third argument for the value of B<salt> which is 13 characters by default. The package implements a
 simple taint checking by 'carping'(warning) for input data which is not any of the following: '!-\/\w\s+"'.
 
 =head1 METHODS
@@ -198,13 +274,16 @@ group id
 =item B<home> -
 home directory
 
-=item B<may> - 
+=item B<may> -
 days before password may be changed
 
 =item B<must> -
 days after which password must be changed
 
-=item B<password> - 
+=item B<name> -
+username
+
+=item B<password> -
 encoded password from shadow file
 
 =item B<shell> -
@@ -213,12 +292,12 @@ login shell
 =item B<uid> -
 user id
 
-=item B<warn> - 
+=item B<warn> -
 days before password is to expire user is warned
 
 =back
 
-=item B<show> 
+=item B<show>
 
 returns the field specified as its only argument. Equivalent to
 B<change> when one argument is given.
@@ -245,6 +324,7 @@ B<getpwent>(3), B<getpwnam>(3), B<usermod>(8), B<passwd>(8)
 =head1 AUTHOR
 
 Vidul Petrov, vidul@abv.bg
+don't forget the method B<authors> :)
 
 =head1 COPYRIGHT
 
